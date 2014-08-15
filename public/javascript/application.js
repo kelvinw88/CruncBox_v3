a6e31c6b168a4d7078bacf35fd37d157b32d58ae
kelvinw88 <kelvinw88@gmail.com>
2014-08-15 13:24:11 -0700

    fix masory

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-
-  $.getJSON( "api/alive", function( posts ) {
-    var source = $("#post-template").html();
-    var template = Handlebars.compile(source);
-    var context = {posts: posts};
-    var html = template(context);
-    $(".grid").html(html);
-  });
-
-
   //TEXT AREA NO \n
   $('textarea').keypress(function(event) {
   if ((event.keyCode || event.which) == 13) {
@@ -21,9 +11,6 @@ $(document).ready(function() {
     $(this).html(keyed);
   });
   // MASONRY
-  $('.grid').masonry({
-    transitionDuration: 0
-  });
 
   // index page alive post
 
@@ -38,10 +25,56 @@ $(document).ready(function() {
   //   }).appendTo( ".grid" );
   // });
 
+  $.getJSON( "api/alive", function( posts ) {
+    var source = $("#post-template").html();
+    var template = Handlebars.compile(source);
+    var context = {posts: posts};
+    var html = template(context);
+    $(".grid").html(html);
+    $('.grid').masonry({
+      transitionDuration: 0
+    });
+  });
 
 
 
-
-
-
+  // sort drunk post
+  $('#show_drunk').on("click", function(){
+    $.getJSON( "api/drunk", function( data ) {
+      var items = [];
+      $.each( data, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+      $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
+    });
+  });
+  // sort sober Post
+  $('#show_sober').on("click", function(){
+    $.getJSON( "api/sober", function( data ) {
+      var items = [];
+      $.each( data, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+      $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
+    });
+  });
+  // sort high post
+  $('#show_high').on("click", function(){
+    $.getJSON( "api/high", function( data ) {
+      var items = [];
+      $.each( data, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+      $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
+    });
+  });
 });
