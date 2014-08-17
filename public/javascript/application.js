274d7ec54eed794861b120086a06b1bf5d046c16
kelvinw88 <kelvinw88@gmail.com>
2014-08-17 01:28:58 -0700

    sorting display

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,24 +1,47 @@
 $(document).ready(function() {
 
-  function masonryFn(){
+  function set_masonry_fn(){
+    var container = document.querySelector('.grid');
+    var msnry = new Masonry(container, {});
+    return msnry;
+  };
+
+  function masonry_no_animation_fn(){
     $('.grid').masonry({
       transitionDuration: 0
     });
   };
 
+  function enter_disable_fn(){
+    $('textarea').keypress(function(event) {
+    if ((event.keyCode || event.which) == 13) {
+        event.preventDefault();;
+        return false;
+      }
+    });
+    $('textarea').keyup(function() {
+      var keyed = $(this).val().replace(/\n/g, '<br/>');
+      $(this).html(keyed);
+    });
+  };
 
-  //TEXT AREA NO \n
-  $('textarea').keypress(function(event) {
-  if ((event.keyCode || event.which) == 13) {
-      event.preventDefault();;
-      return false;
-    }
-  });
+  function remove_hide_fn() {
+    $(".drunk").parent().removeClass('hide_drunk');
+    $(".sober").parent().removeClass('hide_sober');
+    $(".high").parent().removeClass('hide_high');
+  };
 
-  $('textarea').keyup(function() {
-    var keyed = $(this).val().replace(/\n/g, '<br/>');
-    $(this).html(keyed);
-  });
+  function add_hide_fn() {
+    $(".sober").parent().addClass('hide_sober');
+    $(".high").parent().addClass('hide_high');
+    $(".drunk").parent().addClass('hide_drunk');
+  };
+
+  function show_class_fn(class_name){
+    add_hide_fn();
+    $("."+ class_name).parent().removeClass("hide_" + class_name);
+
+  };
 
   //homepage
   $.getJSON( "api/alive", function( posts ) {
@@ -27,56 +50,60 @@ $(document).ready(function() {
     var context = {posts: posts};
     var html = template(context);
     $(".grid").html(html);
-    masonryFn();
+    set_masonry_fn()
+    masonry_no_animation_fn();
+    enter_disable_fn();
   });
 
-  //drunk
-  var show_drunk = false;
-  $('#show_drunk').on("click", function(e){
-    e.preventDefault();
-    var container = document.querySelector('.grid');
-    var msnry = new Masonry( container);
-    show_drunk = !show_drunk;
-    if (show_drunk == true) {
-      $(".sober").parent().addClass('hide_sober');
-      $(".high").parent().addClass('hide_high');
-    } else {
-      $(".sober").parent().removeClass('hide_sober');
-      $(".high").parent().removeClass('hide_high');
-    }
+  //cick on logo
+  $('#logo').on("click", function(){
+    var msnry = set_masonry_fn();
+    remove_hide_fn();
     msnry.layout();
   });
 
   //drunk
   var show_drunk = false;
-  $('#show_drunk').on("click", function(e){
-    e.preventDefault();
-    var container = document.querySelector('.grid');
-    var msnry = new Masonry( container);
+  $('#show_drunk').on("click", function(){
+    var msnry = set_masonry_fn();
     show_drunk = !show_drunk;
     if (show_drunk == true) {
-      $(".sober").parent().addClass('hide_sober');
-      $(".high").parent().addClass('hide_high');
+      show_high = false;
+      show_sober = false;
+      show_class_fn("drunk");
     } else {
-      $(".sober").parent().removeClass('hide_sober');
-      $(".high").parent().removeClass('hide_high');
+      remove_hide_fn()
     }
     msnry.layout();
   });
 
-  //drunk
-  var show_drunk = false;
-  $('#show_drunk').on("click", function(e){
-    e.preventDefault();
-    var container = document.querySelector('.grid');
-    var msnry = new Masonry( container);
-    show_drunk = !show_drunk;
-    if (show_drunk == true) {
-      $(".sober").parent().addClass('hide_sober');
-      $(".high").parent().addClass('hide_high');
+  //sober
+  var show_sober = false;
+  $('#show_sober').on("click", function(e){
+    var msnry = set_masonry_fn();
+    show_sober = !show_sober;
+    if (show_sober == true) {
+      show_high = false;
+      show_drunk = false;
+      show_class_fn("sober");
     } else {
-      $(".sober").parent().removeClass('hide_sober');
-      $(".high").parent().removeClass('hide_high');
+      remove_hide_fn()
+    }
+    msnry.layout();
+  });
+
+  //high
+  var show_high = false;
+  $('#show_high').on("click", function(e){
+    e.preventDefault();
+    var msnry = set_masonry_fn();
+    show_high = !show_high;
+    if (show_high == true) {
+      show_drunk = false;
+      show_sober = false;
+      show_class_fn("high");
+    } else {
+      remove_hide_fn()
     }
     msnry.layout();
   });
@@ -84,18 +111,3 @@ $(document).ready(function() {
 
 
 });
-
-    //$(".high")&&(".sober").parentdiv("").css(display:none;)
-
-
-
-
-
-  // $.getJSON( "api/alive", function( posts ) {
-  //   var source = $("#post-template").html();
-  //   var template = Handlebars.compile(source);
-  //   var context = {posts: posts};
-  //   var html = template(context);
-  //   $(".grid").html(html);
-  //   masonryFn();
-  // });
