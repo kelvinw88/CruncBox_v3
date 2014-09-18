be55b44d5951b59431429bdbbd203c4c7d7839a9
kelvinw88 <kelvinw88@gmail.com>
2014-09-18 01:52:51 -0700

    heroku setup changes on database.rb

--- config/database.rb
+++ config/database.rb
@@ -1,21 +1,33 @@
 configure do
 
-
-  # configure :development, :test do
-  #   set :database, {
-  #     adapter: "sqlite3",
-  #     database: "db/db.sqlite3"
-  #   }
-  # end
-  #
-  if development?
+  configure :development, :test do
     set :database, {
       adapter: "sqlite3",
       database: "db/db.sqlite3"
     }
-  else
-    set :database, ENV['DATABASE_URL']
   end
+
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'])
+
+    ActiveRecord::Base.establish_connection(
+     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+     :host     => db.host,
+     :username => db.user,
+     :password => db.password,
+     :database => db.path[1..-1],
+     :encoding => 'utf8'
+    )
+  end
+
+  # if development?
+  #   set :database, {
+  #     adapter: "sqlite3",
+  #     database: "db/db.sqlite3"
+  #   }
+  # else
+  #   set :database, ENV['DATABASE_URL']
+  # end
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
